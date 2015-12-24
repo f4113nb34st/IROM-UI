5bd48f7e80158e55015f5e8179e1aca8c9d8802f
f4113nb34st <thefatgamer99@yahoo.com>
2015-12-24 08:55:05 -0600

    Added UI core class that derives from MultiCore.

---
+++ Core/UIMultiCore.cs
@@ -0,0 +1,38 @@
+﻿namespace IROM.Core
+{
+	using System;
+	using IROM.UI;
+	using IROM.Util;
+	
+	/// <summary>
+    /// Abstract base class for multi-threaded <see cref="Core"/>s with UISystems.
+    /// </summary>
+	public abstract class UIMultiCore : MultiCore
+	{
+		protected UIMultiCore(String title) : base(title)
+        {
+			UIFrame = new Frame(WindowObj);
+			UIFrame.OnDirtyChange += (sender, e) => MarkDirty();
+			AutoDirty = false;
+		}
+		
+		protected UIMultiCore(String title, double tickRate) : base(title, tickRate)
+        {
+			UIFrame = new Frame(WindowObj);
+			UIFrame.OnDirtyChange += (sender, e) => MarkDirty();
+			AutoDirty = false;
+		}
+		
+		protected Frame UIFrame;
+		
+		protected override void Tick(double deltaTime)
+		{
+			UIFrame.Tick(deltaTime);
+		}
+		
+        protected override void Render(Image image)
+        {
+        	UIFrame.Render(image);
+        }
+	}
+}
